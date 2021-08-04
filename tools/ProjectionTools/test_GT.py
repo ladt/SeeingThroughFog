e6c514d47c21280fde6503e063d6f31e60009f56
Elad <eladtalbi@gmail.com>
2021-08-04 17:29:28 +0300

    2d boxes labels compared

--- tools/ProjectionTools/test_GT.py
+++ tools/ProjectionTools/test_GT.py
@@ -4,15 +4,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 # import scipy.interpolate as sciint
-import cv2
+# import cv2
 import os
+import shutil
+
+def gt_to_2dbox(in_dir, out_dir):
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir)
+    for file_path in in_dir:
+        file_name = Path(file_path.split('.')[0]).stem
+        new_file_path = os.path.join(out_dir, file_name)
+        with open(file_path, 'r') as in_file:
+            with open(new_file_path, 'w') as out_file:
+                for line in in_file:
+                    box = line.split(' ')[4: 8]
+                    line_box = ' '.join(box)
+                    out_file.write(line_box)
 
 def compare_files(dir1, dir2):
     for file_path1 in dir1:
-        file_name = Path(file_path).stem
+        file_name = Path(file_path1.split('.')[0]).stem
         file_path2 = os.path.join(dir2, file_name)
+        with open(file_path1, 'r') as file1:
+            with open(file_path2, 'r') as file2:
+                if set(file1) != set(file2):
+                    print(file_name)
 
 if __name__ == '__main__':
+    indir1 =
+    indir2 =
+    out_dir1 =
+    out_dir2 =
     gt_to_2dbox(in_dir1, bbox_2d_dir1)
     gt_to_2dbox(in_dir2, bbox_2d_dir2)
     compare_files(bbox_2d_dir1, bbox_2d_dir2)
